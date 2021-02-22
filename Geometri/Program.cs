fb0f912b2cf3e1f53eedaf55c2966a896b17d68c
dubidow <danielskaftenielsen@gmail.com>
2021-02-22 12:22:20 +0100

    Add project files.

---
+++ Geometri/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Geometri
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Square square = new Square(laengde: 5000);
+            Console.WriteLine(square.Omkreds());
+
+            square.SetSide(laengde: 250);
+            Console.WriteLine(square.Omkreds());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Geometri/square.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Geometri
+{
+   public class Square
+    {
+        private double Side { get; set; }
+
+       /* public void SetSide(double laengde, int v)
+        {
+            Side = laengde;
+        }
+       */
+        internal void SetSide(int laengde)
+        {
+            Side = laengde;
+        }
+
+        public Square (double laengde)
+        {
+            Side = laengde;
+        }
+
+        public double Omkreds()
+        {
+            return 4 * Side;
+        }
+
+    }
+}
